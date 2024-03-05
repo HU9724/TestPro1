8067c1258b95f9f2ff983a3c2ec914404f5c0f31
HU9724 <dhlee9724@naver.com>
2024-03-05 10:29:35 +0900

    sasa

--- src/main/java/ch01/test01.java
+++ src/main/java/ch01/test01.java
@@ -4,11 +4,9 @@ public class test01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
-		System.out.println("==================123444444444");
-=======
-		System.out.println("==================123");
->>>>>>> branch 'hu' of https://github.com/HU9724/TestPro1.git
+
+		System.out.println("==================12344444444411111111");
+
 	}
 
 }
